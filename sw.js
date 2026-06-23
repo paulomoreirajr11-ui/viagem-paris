6b0586df507268ed7ae101ade34c7aeb777fc2c6
Paulo Moreira Junior <paulo.moreirajr11@gmail.com>
2026-06-23 10:44:59 -0300

    Atalho discreto pro roteiro de video no rodape da home + botao 'voltar ao guia' no roteiro (cache v23)

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-var CACHE='guia-paris-v22';
+var CACHE='guia-paris-v23';
 var CORE=['./','./index.html','./roteiro-video.html','./manifest.webmanifest','./img/icon.svg'];
 self.addEventListener('install',function(e){e.waitUntil(caches.open(CACHE).then(function(c){return c.addAll(CORE);}).then(function(){return self.skipWaiting();}));});
 self.addEventListener('activate',function(e){e.waitUntil(caches.keys().then(function(ks){return Promise.all(ks.map(function(k){if(k!==CACHE)return caches.delete(k);}));}).then(function(){return self.clients.claim();}));});
